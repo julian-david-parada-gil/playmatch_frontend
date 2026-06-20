1235e7d2227be783800331c2d3e6954ab9fb47cd
julian <jparadaa1707@gmail.com>
2026-06-20 10:12:20 -0500

    chore: se cambio la pagina del home y en la ladding puse admingrupo falta es agregarlo por defecto

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -11,11 +11,7 @@ export const Home = () => {
 
   return (
     <Row>
-      <Col md="3" className="pad">
-        <span className="hipster rounded" />
-      </Col>
       <Col md="9">
-        <h1 className="display-4">¡Bienvenido, Java Hipster!</h1>
         <p className="lead">Esta es su página de inicio</p>
         {account?.login ? (
           <div>
@@ -30,8 +26,9 @@ export const Home = () => {
                 iniciar sesión
               </Link>
               , puede intentar con las cuentas predeterminadas:
-              <br />- Administrador (usuario=&quot;admin&quot; y contraseña=&quot;admin&quot;) <br />- Usuario (usuario=&quot;user&quot; y
-              contraseña=&quot;user&quot;).
+              <br />- Administrador (usuario=&quot;admin&quot; y contraseña=&quot;admin&quot;)
+              <br />- Usuario (usuario=&quot;user&quot; y contraseña=&quot;user&quot;).
+              <br />- Administrador de Grupo (usuario="admingrupo"; y contraseña="admingrupo")
             </Alert>
 
             <Alert variant="warning">
@@ -42,43 +39,6 @@ export const Home = () => {
             </Alert>
           </div>
         )}
-        <p>Si tiene preguntas sobre JHipster:</p>
-
-        <ul>
-          <li>
-            <a href="https://www.jhipster.tech/" target="_blank" rel="noopener noreferrer">
-              Página de inicio de JHipster
-            </a>
-          </li>
-          <li>
-            <a href="https://stackoverflow.com/tags/jhipster/info" target="_blank" rel="noopener noreferrer">
-              JHipster en Stack Overflow
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/jhipster/generator-jhipster/issues?state=open" target="_blank" rel="noopener noreferrer">
-              JHipster seguimiento de errores
-            </a>
-          </li>
-          <li>
-            <a href="https://gitter.im/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-              Sala de chat pública de JHipster
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/jhipster" target="_blank" rel="noopener noreferrer">
-              contacto @jhipster en Twitter
-            </a>
-          </li>
-        </ul>
-
-        <p>
-          Si te gusta JHipster, danos una estrella en{' '}
-          <a href="https://github.com/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-            GitHub
-          </a>
-          !
-        </p>
       </Col>
     </Row>
   );
