e709fd46fce06ad6afce7948637056cc4b816909
Jackson Estid Roa Gomez <jacksonroagomez@gmail.com>
2026-06-30 14:59:13 -0500

    feat: proteccion de rutas en Grupo

--- src/main/webapp/app/entities/grupo/index.tsx
+++ src/main/webapp/app/entities/grupo/index.tsx
@@ -7,15 +7,38 @@ import Grupo from './grupo';
 import GrupoDeleteDialog from './grupo-delete-dialog';
 import GrupoDetail from './grupo-detail';
 import GrupoUpdate from './grupo-update';
+import PrivateRoute from 'app/shared/auth/private-route';
+import { Authority } from 'app/shared/jhipster/constants';
 
 const GrupoRoutes = () => (
   <ErrorBoundaryRoutes>
     <Route index element={<Grupo />} />
-    <Route path="new" element={<GrupoUpdate />} />
+    <Route
+      path="new"
+      element={
+        <PrivateRoute hasAnyAuthorities={[Authority.ADMIN, Authority.ADMINGRUPO]}>
+          <GrupoUpdate />
+        </PrivateRoute>
+      }
+    />
     <Route path=":id">
       <Route index element={<GrupoDetail />} />
-      <Route path="edit" element={<GrupoUpdate />} />
-      <Route path="delete" element={<GrupoDeleteDialog />} />
+      <Route
+        path="edit"
+        element={
+          <PrivateRoute hasAnyAuthorities={[Authority.ADMIN, Authority.ADMINGRUPO]}>
+            <GrupoUpdate />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="delete"
+        element={
+          <PrivateRoute hasAnyAuthorities={[Authority.ADMIN, Authority.ADMINGRUPO]}>
+            <GrupoDeleteDialog />
+          </PrivateRoute>
+        }
+      />
     </Route>
   </ErrorBoundaryRoutes>
 );
