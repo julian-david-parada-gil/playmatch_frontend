2a768f7025d68028893da2504fc556ff4bca97d4
Jackson Estid Roa Gomez <jacksonroagomez@gmail.com>
2026-06-30 14:06:07 -0500

    feat: proteccion de rutas en Tipo-Documento

--- src/main/webapp/app/entities/tipo-documento/index.tsx
+++ src/main/webapp/app/entities/tipo-documento/index.tsx
@@ -7,15 +7,38 @@ import TipoDocumento from './tipo-documento';
 import TipoDocumentoDeleteDialog from './tipo-documento-delete-dialog';
 import TipoDocumentoDetail from './tipo-documento-detail';
 import TipoDocumentoUpdate from './tipo-documento-update';
+import PrivateRoute from 'app/shared/auth/private-route';
+import { Authority } from 'app/shared/jhipster/constants';
 
 const TipoDocumentoRoutes = () => (
   <ErrorBoundaryRoutes>
     <Route index element={<TipoDocumento />} />
-    <Route path="new" element={<TipoDocumentoUpdate />} />
+    <Route
+      path="new"
+      element={
+        <PrivateRoute hasAnyAuthorities={[Authority.ADMIN]}>
+          <TipoDocumentoUpdate />
+        </PrivateRoute>
+      }
+    />
     <Route path=":id">
       <Route index element={<TipoDocumentoDetail />} />
-      <Route path="edit" element={<TipoDocumentoUpdate />} />
-      <Route path="delete" element={<TipoDocumentoDeleteDialog />} />
+      <Route
+        path="edit"
+        element={
+          <PrivateRoute hasAnyAuthorities={[Authority.ADMIN]}>
+            <TipoDocumentoUpdate />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="delete"
+        element={
+          <PrivateRoute hasAnyAuthorities={[Authority.ADMIN]}>
+            <TipoDocumentoDeleteDialog />
+          </PrivateRoute>
+        }
+      />
     </Route>
   </ErrorBoundaryRoutes>
 );
