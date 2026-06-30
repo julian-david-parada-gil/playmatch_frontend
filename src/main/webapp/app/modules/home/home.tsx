107fb68a4056e4d6106d41cc78b68d2ef111b86b
julian <jparadaa1707@gmail.com>
2026-06-30 12:43:44 -0500

    chore: reestuctura front

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -11,8 +11,25 @@ export const Home = () => {
 
   return (
     <Row>
-      <Col md="9">
-        <p className="lead">Bienvenido a PlayMatch</p>
+      {/* Columna izquierda: Logo */}
+      <Col md="4" className="d-flex align-items-center justify-content-center">
+        <img src="content/images/playmatch.png" alt="Logo PlayMatch" style={{ maxWidth: '100%', height: 'auto' }} />
+      </Col>
+
+      {/* Columna derecha: Título y texto */}
+      <Col md="8">
+        <h2>Bienvenido a PlayMatch</h2>
+
+        <Alert variant="warning">
+          <h4 className="alert-heading">Sobre PlayMatch</h4>
+          <p>Somos una plataforma diseñada para la gestión integral de torneos deportivos en Colombia.</p>
+          <hr />
+          <p className="mb-0">
+            <strong>Objetivo principal:</strong> Facilitamos la organización de partidos en disciplinas como fútbol en modalidades 8x8,
+            11x11, microfutbol o fubol de salon 5x5, baloncesto 5x5, entre otras disciplinas.
+          </p>
+        </Alert>
+
         {account?.login ? (
           <div>
             <Alert variant="success">Está conectado como &quot;{account.login}&quot;.</Alert>
