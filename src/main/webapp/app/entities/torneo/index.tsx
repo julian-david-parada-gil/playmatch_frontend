d9c3b01bc5a28ba66e0e25fe5b1b8b75d2c4d383
Jackson Estid Roa Gomez <jacksonroagomez@gmail.com>
2026-06-30 13:19:25 -0500

    feat: proteccion de rutas en Torneo

--- src/main/webapp/app/entities/torneo/index.tsx
+++ src/main/webapp/app/entities/torneo/index.tsx
@@ -7,15 +7,38 @@ import Torneo from './torneo';
 import TorneoDeleteDialog from './torneo-delete-dialog';
 import TorneoDetail from './torneo-detail';
 import TorneoUpdate from './torneo-update';
+import PrivateRoute from 'app/shared/auth/private-route';
+import { Authority } from 'app/shared/jhipster/constants';
 
 const TorneoRoutes = () => (
   <ErrorBoundaryRoutes>
     <Route index element={<Torneo />} />
-    <Route path="new" element={<TorneoUpdate />} />
+    <Route
+      path="new"
+      element={
+        <PrivateRoute hasAnyAuthorities={[Authority.ADMIN]}>
+          <TorneoUpdate />
+        </PrivateRoute>
+      }
+    />
     <Route path=":id">
       <Route index element={<TorneoDetail />} />
-      <Route path="edit" element={<TorneoUpdate />} />
-      <Route path="delete" element={<TorneoDeleteDialog />} />
+      <Route
+        path="edit"
+        element={
+          <PrivateRoute hasAnyAuthorities={[Authority.ADMIN]}>
+            <TorneoUpdate />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="delete"
+        element={
+          <PrivateRoute hasAnyAuthorities={[Authority.ADMIN]}>
+            <TorneoDeleteDialog />
+          </PrivateRoute>
+        }
+      />
     </Route>
   </ErrorBoundaryRoutes>
 );
