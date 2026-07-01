5b642c4b4c7dbe7bac2dd9fa61439ac628168bf4
jackson-estid <jacksonroagomez@gmail.com>
2026-06-30 19:15:33 -0500

    feat: proteccion de rutas Noticia

--- src/main/webapp/app/entities/noticia/index.tsx
+++ src/main/webapp/app/entities/noticia/index.tsx
@@ -7,15 +7,38 @@ import Noticia from './noticia';
 import NoticiaDeleteDialog from './noticia-delete-dialog';
 import NoticiaDetail from './noticia-detail';
 import NoticiaUpdate from './noticia-update';
+import PrivateRoute from 'app/shared/auth/private-route';
+import { Authority } from 'app/shared/jhipster/constants';
 
 const NoticiaRoutes = () => (
   <ErrorBoundaryRoutes>
     <Route index element={<Noticia />} />
-    <Route path="new" element={<NoticiaUpdate />} />
+    <Route
+      path="new"
+      element={
+        <PrivateRoute hasAnyAuthorities={[Authority.ADMIN, AuthorityORGANIZADOR, AuthorityADMINGRUPO]}>
+          <NoticiaUpdate />
+        </PrivateRoute>
+      }
+    />
     <Route path=":id">
       <Route index element={<NoticiaDetail />} />
-      <Route path="edit" element={<NoticiaUpdate />} />
-      <Route path="delete" element={<NoticiaDeleteDialog />} />
+      <Route
+        path="edit"
+        element={
+          <PrivateRoute hasAnyAuthorities={[Authority.ADMIN, AuthorityORGANIZADOR, AuthorityADMINGRUPO]}>
+            <NoticiaUpdate />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="delete"
+        element={
+          <PrivateRoute hasAnyAuthorities={[Authority.ADMIN, AuthorityORGANIZADOR, AuthorityADMINGRUPO]}>
+            <NoticiaDeleteDialog />
+          </PrivateRoute>
+        }
+      />
     </Route>
   </ErrorBoundaryRoutes>
 );
