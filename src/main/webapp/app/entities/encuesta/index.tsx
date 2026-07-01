b2a2ac2065dc3d73664bc955379d2b9e6652ba1d
jackson-estid <jacksonroagomez@gmail.com>
2026-06-30 19:26:32 -0500

    feat: proteccion de rutas Encuesta

--- src/main/webapp/app/entities/encuesta/index.tsx
+++ src/main/webapp/app/entities/encuesta/index.tsx
@@ -7,15 +7,38 @@ import Encuesta from './encuesta';
 import EncuestaDeleteDialog from './encuesta-delete-dialog';
 import EncuestaDetail from './encuesta-detail';
 import EncuestaUpdate from './encuesta-update';
+import PrivateRoute from 'app/shared/auth/private-route';
+import { Authority } from 'app/shared/jhipster/constants';
 
 const EncuestaRoutes = () => (
   <ErrorBoundaryRoutes>
     <Route index element={<Encuesta />} />
-    <Route path="new" element={<EncuestaUpdate />} />
+    <Route
+      path="new"
+      element={
+        <PrivateRoute hasAnyAuthorities={[Authority.ADMIN, AuthorityORGANIZADOR, AuthorityADMINGRUPO]}>
+          <EncuestaUpdate />
+        </PrivateRoute>
+      }
+    />
     <Route path=":id">
       <Route index element={<EncuestaDetail />} />
-      <Route path="edit" element={<EncuestaUpdate />} />
-      <Route path="delete" element={<EncuestaDeleteDialog />} />
+      <Route
+        path="edit"
+        element={
+          <PrivateRoute hasAnyAuthorities={[Authority.ADMIN, AuthorityORGANIZADOR, AuthorityADMINGRUPO]}>
+            <EncuestaUpdate />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="delete"
+        element={
+          <PrivateRoute hasAnyAuthorities={[Authority.ADMIN, AuthorityORGANIZADOR, AuthorityADMINGRUPO]}>
+            <EncuestaDeleteDialog />
+          </PrivateRoute>
+        }
+      />
     </Route>
   </ErrorBoundaryRoutes>
 );
